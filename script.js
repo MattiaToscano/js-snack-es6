aec4a73d06df3e451f2928731d33f80cc076333e
MattiaToscano <mattia1997ct@gmail.com>
2025-03-28 14:09:02 +0100

    [UPD] Creato array di oggetti

---
+++ script.js
@@ -0,0 +1,20 @@
+//Esercizio 1
+
+//Creare un array di oggetti: ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e peso.
+
+const bici = [
+    {
+        nome: "Bugatti",
+        peso: 10
+    },
+    {
+        nome: "Ferrari",
+        peso: 12
+    },
+    {
+        nome: "Lamborghini",
+        peso: 8
+    }
+];
+
+//Stampare in console la bici con il peso minore
